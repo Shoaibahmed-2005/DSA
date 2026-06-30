5821b5bce163c8ed38e7a1348e1ea76c8ee08aa3
SHOAIB AHMED SHERIFF <shoaibahmedsheriff2005@gmail.com>
2026-07-01 02:43:16 +0530

    Added solution - LeetHub

---
+++ 702382-sort-0s-1s-and-2s-GFG/702382-sort-0s-1s-and-2s.py
@@ -0,0 +1,22 @@
+class Solution:
+    def sort012(self, arr):
+        low = 0
+        mid = 0
+        high = len(arr) - 1
+
+        while mid <= high:
+            if arr[mid] == 0:
+                arr[low], arr[mid] = arr[mid], arr[low]
+                low += 1
+                mid += 1
+
+            elif arr[mid] == 1:
+                mid += 1
+
+            else:  # arr[mid] == 2
+                arr[mid], arr[high] = arr[high], arr[mid]
+                high -= 1
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
