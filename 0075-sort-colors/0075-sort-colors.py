566a36bce3085090f32a29774e3023c41051a139
SHOAIB AHMED SHERIFF <shoaibahmedsheriff2005@gmail.com>
2026-07-02 19:58:44 +0530

    Time: 0 ms (100%), Space: 19.2 MB (89.78%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,23 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n=len(nums)
+        low,mid,high=0,0,n-1
+        while mid<=high:
+            if nums[mid]==0:
+                nums[low],nums[mid]=nums[mid],nums[low]
+                low+=1
+                mid+=1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                nums[mid],nums[high]=nums[high],nums[mid]
+                high-=1
+                
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
