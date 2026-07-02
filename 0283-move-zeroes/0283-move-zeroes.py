1aabcc0593372e25c58aa98c72d9c28386831383
SHOAIB AHMED SHERIFF <shoaibahmedsheriff2005@gmail.com>
2026-07-02 20:48:44 +0530

    Time: 5 ms (50%), Space: 20.5 MB (23.76%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,7 +3,7 @@ class Solution:
         i = 0
 
         for j in range(len(nums)):
-            if nums[j]:
+            if nums[j] != 0:
                 nums[i], nums[j] = nums[j], nums[i]
                 i += 1
 
