e0c2bf4871da65b9844bdacf2b546be8ef0da1da
SHOAIB AHMED SHERIFF <shoaibahmedsheriff2005@gmail.com>
2026-07-03 23:34:08 +0530

    Added solution - LeetHub

---
+++ 703836-max-sum-subarray-of-size-k-GFG/703836-max-sum-subarray-of-size-k.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxSubarraySum(self, arr, k):
+        n = len(arr)
+
+        window_sum = sum(arr[:k])
+        max_sum = window_sum
+
+        for i in range(k, n):
+            window_sum += arr[i] - arr[i - k]
+            max_sum = max(max_sum, window_sum)
+
+        return max_sum
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
