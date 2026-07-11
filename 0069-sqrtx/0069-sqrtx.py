40de32b0903e0663ccfac2664fbff5bcdcb61a74
SHOAIB AHMED SHERIFF <shoaibahmedsheriff2005@gmail.com>
2026-07-11 23:55:37 +0530

    Time: 4 ms (43.27%), Space: 19.2 MB (57.79%) - LeetHub - Time: O(, Space: O(?)

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,27 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+
+        if x < 2:
+            return x
+
+        left, right = 1, x // 2
+
+        while left <= right:
+
+            mid = (left + right) // 2
+            square = mid * mid
+
+            if square == x:
+                return mid
+
+            if square < x:
+                left = mid + 1
+
+            else:
+                right = mid - 1
+
+        return right
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
